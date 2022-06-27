9c64d649a603eaaa5a262d8d1f974eef7cd6cc27
ego-edesse <61778330+ego-edesse@users.noreply.github.com>
2022-06-28 00:50:39 +0200

    Adding a basic ASCII generator
    Adding a basic ASCII generator evaluating grayscale values for 1/n (where n is the factor for the final resolution) pixels only.
    Output only in the Text widget, external .txt support will be added shortly after

---
+++ main.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+import math
+from PIL import Image
+
+charset = ['#', 'm', 'c', 'f', '1', '^', ':', "."]
+
+def load_image():
+    print("Loading image")
+    filename = fd.askopenfilename()
+    global loaded_image_path
+    global load_image_label
+
+    loaded_image_path = filename
+    load_image_label.configure(text=loaded_image_path)
+
+
+def generate_ascii():
+    print("Generating ASC-II")
+    global loaded_image_path
+    global size_scale
+
+    with Image.open(loaded_image_path) as im:
+        im = im.convert('L')
+        output_string = ""
+        output_scaling = int((100/size_scale.get()))
+
+        for i in range(0, im.height, output_scaling):
+            for j in range(0, im.width, output_scaling):
+                lum = im.getpixel((j, i))/255
+                try:
+                    value = charset[math.ceil(lum*len(charset))-1]
+                except IndexError:
+                    print(f"Error for i={i}, j={j}:\nvalue = charset[math.floor({lum}*{len(charset)}]\n"
+                          f"-> value = charset[math.floor({lum*len(charset)})")
+                output_string += value
+            output_string += "\n"
+
+        #cleaning previous output
+        ascii_output.delete(1.0, tk.END)
+
+        #resizing text widget to new size for output
+        ascii_output.configure(width=math.ceil(im.width/output_scaling), height=math.floor(im.height/output_scaling))
+
+        #insert new text
+        ascii_output.insert(tk.END, output_string)
+
+        im.close()
+
+
+
+
+
+loaded_image_path = ""
+
+root = tk.Tk()
+root.title("Pic to ASC-II")
+root.geometry("1200x800")
+
+main_frame = tk.Frame(root)
+main_frame.pack(expand=True)
+
+left_frame = tk.Frame(main_frame)
+left_frame.grid(row=0, column=0)
+
+load_image_button = tk.Button(left_frame)
+load_image_button.configure(text="Load Image", command=lambda: load_image())
+load_image_button.grid(row=0, column=0, pady=2)
+
+load_image_label = tk.Label(left_frame)
+load_image_label.configure(text="<...>")
+load_image_label.grid(row=1, column=0, pady=0)
+
+size_scale = tk.Scale(left_frame, length=120, tickinterval=100, label="output size (in %)", from_=1,
+                      orient="horizontal", sliderlength=10)
+size_scale.set(10)
+size_scale.grid(row=2, column=0, pady=4)
+
+generate_image_button = tk.Button(left_frame)
+generate_image_button.configure(text="Generate ASC-II", command=lambda: generate_ascii())
+generate_image_button.grid(row=3, column=0, pady=6)
+
+ascii_frame = tk.Frame(main_frame)
+ascii_frame.configure(relief="sunken", borderwidth=1)
+ascii_frame.grid(row=0, column=1, padx=20, pady=12)
+ascii_frame.grid_anchor("center")
+
+ascii_output = tk.Text(ascii_frame)
+ascii_output.configure(width=50, height=20)
+ascii_output.pack()
+
+
+root.mainloop()
+
+
