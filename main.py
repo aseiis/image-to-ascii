ee1c69b7db763dc7f7689aca1df9a0becbb3ce62
ego-edesse <61778330+ego-edesse@users.noreply.github.com>
2022-06-29 17:23:05 +0200

    update main script
    Added a support for big output and message box to warn user when output isn't possible in preview

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 import tkinter.filedialog as fd
+import tkinter.messagebox as mb
 import math
 from PIL import Image
 
 charset = ['#', 'm', 'c', 'f', '1', '^', ':', "."]
+
+default_font = ("Courier", 10)
+small_default_font = ("Courier", 5)
 
 def load_image():
     print("Loading image")
@@ -36,14 +40,27 @@ def generate_ascii():
                 output_string += value
             output_string += "\n"
 
-        #cleaning previous output
+        # cleaning previous output
         ascii_output.delete(1.0, tk.END)
 
-        #resizing text widget to new size for output
-        ascii_output.configure(width=math.ceil(im.width/output_scaling), height=math.floor(im.height/output_scaling))
+        # prevents too large output
+        if im.width/output_scaling > 200 or im.height/output_scaling > 100:
+            infobox = mb.showinfo("Output too large", message="Output is too big for preview mode. Please use the "
+                                                              ".txt file output method when generating ASC-II or "
+                                                              "use a smaller resolution.")
+        else:
+            # resizing text widget to new size for output
+            ascii_output.configure(width=math.ceil(im.width / output_scaling),
+                                   height=math.floor(im.height / output_scaling))
 
-        #insert new text
-        ascii_output.insert(tk.END, output_string)
+            # insert new text
+            ascii_output.insert(tk.END, output_string)
+
+            # zoom-out if large output
+            if im.width/output_scaling > 100 or im.height/output_scaling > 50:
+                ascii_output.configure(font=small_default_font)
+            else:
+                ascii_output.configure(font=default_font)
 
         im.close()
 
@@ -86,7 +103,7 @@ ascii_frame.grid(row=0, column=1, padx=20, pady=12)
 ascii_frame.grid_anchor("center")
 
 ascii_output = tk.Text(ascii_frame)
-ascii_output.configure(width=50, height=20)
+ascii_output.configure(width=50, height=20, font=default_font)
 ascii_output.pack()
 
 
